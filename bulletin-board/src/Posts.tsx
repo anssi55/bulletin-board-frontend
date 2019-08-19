659a574489370b6a7d215eadcf01cee1260eaf12
Anssi Laurento <anssi.laurento@gmail.com>
2019-08-19 17:14:04 +0300

    added loading and error messages

--- bulletin-board/src/Posts.tsx
+++ bulletin-board/src/Posts.tsx
@@ -1,44 +1,70 @@
-import React from "react";
+import React from 'react';
 
-const API = "http://localhost:4000/api/v1/posts";
-interface Props {}
+const API = 'http://localhost:4000/api/v1/posts';
+type IProps = {};
 
-interface State {
+type IState = {
   posts: Post[];
-}
-interface Post {
+  isLoading: boolean;
+  errors: any;
+};
+type Post = {
   id: number;
   post: string;
   topic: string;
   datetime: Date;
   pinned: boolean;
   modified: Date;
-}
+};
 
-class Posts extends React.Component<Props, State> {
-  state: State = {
-    posts: []
-  };
+class Posts extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      posts: [],
+      isLoading: true,
+      errors: null
+    };
+  }
   componentDidMount() {
-    this.getAndSetPosts();
+    setTimeout(this.getAndSetPosts, 5000);
   }
   getAndSetPosts = () => {
+    this.setState({ isLoading: true, errors: null });
     fetch(API)
-      .then(response => response.json())
-      .then(data => this.setState({ posts: data }))
-      .catch(error => console.log(error));
+      .then(response => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        } else {
+          return response.json();
+        }
+      })
+      .then(data => this.setState({ posts: data, isLoading: false }))
+      .catch(errors => this.setState({ errors, isLoading: false }));
   };
 
   render() {
+    const { posts, isLoading, errors } = this.state;
+
+    if (isLoading) {
+      return <p>Loading posts...</p>;
+    }
+    if (errors) {
+      return (
+        <div className="errorContainer">
+          <p>Loading posts failed!</p>
+          <button onClick={this.getAndSetPosts}>Try again</button>
+        </div>
+      );
+    }
+
     return (
       <div className="container">
         <h1>Epic posts</h1>
-        {this.state.posts.map(post => (
-          <div className="posts">
-            <div className="post-body">
-              <h5 className="post-topic">{post.topic}</h5>
-              <h6 className="post-post">{post.post}</h6>
-            </div>
+        {posts.map(post => (
+          <div key={post.id} className="posts">
+            <h5 className="post-topic">{post.topic}</h5>
+            <h6 className="post-post">{post.post}</h6>
           </div>
         ))}
       </div>
